d5161b5b1935db91d13d4eb36e4c086d8c80d015
unknown <maryonyi144@gmail.com>
2021-11-03 13:11:18 -0400

    made some changes to the baseUrl

--- src/data/weatherapi.js
+++ src/data/weatherapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
 const apiKey = 'a057af28704b2881f4b469f7187020d8';
 
  export const getWeatherData =  async (cityname) => {
